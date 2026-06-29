7caf22acb9ba298cc217b27ecbb2c74b16f2dd42
MikeVasovsky <pptestkrylov@gmail.com>
2026-06-29 21:18:42 +0300

    Правки для запуска тестов по тэгу

--- src/test/java/tests/CaseTest.java
+++ src/test/java/tests/CaseTest.java
@@ -2,15 +2,17 @@ package tests;
 
 import model.CreateCaseModel;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+@Tag("case")
 public class CaseTest extends BaseTest{
 
     @Test
     @DisplayName("Проверка добавления нового ежедневного дела")
     void addEverydayCase(){
         CreateCaseModel model = actions.createEveryDayCaseData();
-        authSteps.preSaveUserLogin();
+        authSteps.registrationAndLogin();
         actions.getMainPage().goToCreateCase()
                 .createCase(model)
                 .checkCreateCase(model.getTittle());
