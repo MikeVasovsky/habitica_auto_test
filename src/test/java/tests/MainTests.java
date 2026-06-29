c8e57b241daa653d045407fe50dbb7b06c80c441
MikeVasovsky <pptestkrylov@gmail.com>
2026-06-29 22:29:20 +0300

    добавил тэг main_test

--- src/test/java/tests/MainTests.java
+++ src/test/java/tests/MainTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import data.TaskData;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -11,6 +12,7 @@ import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
+@Tag("main_test")
 public class MainTests extends BaseTest {
 
     private static Stream<Arguments> getTaskElementst() {
